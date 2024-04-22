84b7dbb5bb36dde971d3c1f66bb5aebcd3fdf5ec
vesaun <vesh9653@colorado.edu>
2024-04-21 19:58:22 -0600

    wrote getMovies API

--- code/script.js
+++ code/script.js
@@ -1,17 +1,25 @@
-const axios = require('axios');
+app.get('/getMovies', async (req, res) => {
 
-const options = {
-  method: 'GET',
-  url: 'https://moviesdatabase.p.rapidapi.com/titles',
-  headers: {
-    'X-RapidAPI-Key': 'b4c3222b69msh063d7ee46dfbdb7p1fd1dfjsn0e10ed3e5cfc',
-    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-  }
-};
+  try {
+    const axios = require('axios');
 
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+    const options = {
+      method: 'GET',
+      url: 'https://moviesdatabase.p.rapidapi.com/titles',
+      headers: {
+        'X-RapidAPI-Key': 'b4c3222b69msh063d7ee46dfbdb7p1fd1dfjsn0e10ed3e5cfc',
+        'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+      }
+    };
+  
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  res.render('pages/discover', { results });
+  } catch (error) {
+    // Handle errors and render discover.ejs with an empty results array and an error message
+    res.render('pages/discover', { results: [], error: 'Failed to fetch events from Ticketmaster API.' });
+  }});
